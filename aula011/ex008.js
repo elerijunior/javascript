a09e1dbebc18c5cc5346911d5f6ed8c5c5beaaa5
elerijunior <107778775+elerijunior@users.noreply.github.com>
2024-07-15 16:46:50 -0300

    condições em JS

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40.1
+console.log(`A velocidade do seu carro é ${vel} km`)
+if( vel > 40){ // condição simples
+    console.log(`Você excedeu a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre com segurança`)
