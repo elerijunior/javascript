13198e517328c28617687e8a00f8707baf3d10f6
elerijunior <107778775+elerijunior@users.noreply.github.com>
2024-07-16 16:23:55 -0300

    mais condições e switch case

---
+++ aula012/ex012-1.js
@@ -0,0 +1,10 @@
+var agr = new Date()
+var hora = agr.getHours()
+console.log(`São exatamente ${hora} horas!`)
+    if( hora <= 12){
+        console.log(`Bom dia!`)
+    }else if(hora > 12 || hora <= 18){
+        console.log(`Boa tarde!`)
+    }else if(hora > 18){
+        console.log(`Boa noite!`)
+    }
