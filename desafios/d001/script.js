5d7bb87cfe60ddfe7fbe9d2ecb205a95f88a11c1
elerijunior <107778775+elerijunior@users.noreply.github.com>
2024-07-17 20:36:54 -0300

    resolvendo desafio 1

---
+++ desafios/d001/script.js
@@ -0,0 +1,11 @@
+function clicou(id){
+    if(id === 'botao1'){
+        alert('Você clicou no Botão 1')
+    } else if (id === 'botao2'){
+        alert('Você clicou no Botão 2')
+    }   else if (id === 'botao3'){
+        alert('Você clicou no Botão 3')
+    }
+
+}
+
