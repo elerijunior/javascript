bb80d0345a10220e8fc67753feddcd1befbc001a
elerijunior <107778775+elerijunior@users.noreply.github.com>
2024-07-19 19:09:45 -0300

    organizando arquivos e introdução a estruturas de repetição

---
+++ aula013/ambiente.js
@@ -0,0 +1,11 @@
+var cond = 1
+
+do{
+    console.log(`Passo ${cond}`)
+    cond++
+}while(cond <=5)
+
+/*while(cond <= 6){
+    console.log(`Passo ${cond}`)
+    cond++
+} */
---
+++ aula013/node_5bab3915708f5.tmp.js
@@ -0,0 +1,5 @@
+var cond = 1
+
+while(cond <= 6){
+    console.log(`Passo ${cond}`)
+}
