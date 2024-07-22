4f3a27cfa90a55cc31e7a1ed05f76b7e0e3fee8b
elerijunior <107778775+elerijunior@users.noreply.github.com>
2024-07-22 16:42:32 -0300

    resolução completa ex014 repetições

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -1,12 +1,27 @@
 function contar(){
-    var ini = window.document.querySelector('input#ini')
-    var fim = window.document.querySelector('input#fim')
-    var incre = window.document.querySelector('input#incre')
-    var res = window.document.querySelector('div#res')
-    if(ini.value.length == 0 || fim.value.length == 0 || incre.value.length == 0){
-        res.innerHTML = 'Impossível contar...'
+    //acesso valores(.value) digitado pelo inimigo
+    let ini = window.document.querySelector('input#txti').value
+    let fim = window.document.querySelector('input#txtf').value
+    let pas = window.document.querySelector('input#txtp').value
+    let res = window.document.querySelector('div#res')
+    //transformo os dados dos inimigo em numeros inteiros
+    let numI = parseInt(ini)
+    let numF = parseInt(fim)
+    let numP = parseInt(pas)
+    // verifico se a anta não deixou nada em branco
+    if(isNaN(numI) || isNaN(numF)|| isNaN(numP)){
+        alert(`Está faltando dados. Por favor preencha os campos corretamente.`)
     }
-    for (var i = ini; i <= fim; i = incre){
-        res.innerHTML = `<p>Contando:</p>\n${res}...`
+        res.innerHTML = `Contando...<br>`
+        //executa a contagem de forma progressiva
+        if( numI <= numF){
+            for(let i = numI; i <= numF ; i = i + numP){ 
+                res.innerHTML += `${i} \u{1F449}`
+            }
+        //executa a contagem de forma decrescente    
+        } else {
+            for(let i = numI; i >= numF ; i = i - numP){
+                res.innerHTML += `${i} \u{1F449}`
+            }
     }
 }
