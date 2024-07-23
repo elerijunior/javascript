cdcca5bb84459b5e0994b3e2589e08d28b0078fb
elerijunior <107778775+elerijunior@users.noreply.github.com>
2024-07-22 21:47:16 -0300

    resolução da segunda atividade usando repetições, appChield element

--- aula014ex/ex017/script.js
+++ aula014ex/ex017/script.js
@@ -1,1 +1,17 @@
-alert('Olá')
+function gerar() {
+    let num = document.querySelector('input#num').value
+    let tab = document.querySelector('select#seltab')
+    let valor = parseInt(num)
+    if(isNaN(valor)){
+        alert('Digite um número entre 1 e 9.')
+    } else {
+        let i = 1
+        tab.innerHTML = ''
+        while(i <= 10){
+            let item = document.createElement('option')
+            item.text = `${valor} x ${i} = ${valor*i}`
+            tab.appendChild(item)
+            i++
+        }
+    }
+}
